0876559c8798c1d7e02139b6aa685eaab3f0b554
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-10-22 11:51:29 -0300

    escrevendo testes unitários para o service: leitura de carros

--- src/tests/unit/services/car.test.ts
+++ src/tests/unit/services/car.test.ts
@@ -12,6 +12,10 @@ describe('Car service', () => {
   before(async () => {
     sinon
       .stub(carModel, 'create').resolves(carMockWithId)
+    sinon
+      .stub(carModel, 'read').resolves()
+    sinon
+      .stub(carModel, 'readOne').resolves(carMockWithId)
   });
 
   after(()=>{
@@ -22,6 +26,20 @@ describe('Car service', () => {
     it('success', async () => {
       const carCreated = await carService.create(carMock);
       expect(carCreated).to.be.deep.equal(carMockWithId);
+    });
+  });
+
+  describe('read cars', () => {
+    it('success', async () => {
+      const cars = await carService.read();
+      expect(cars).to.be.deep.equal([carMockWithId]);
+    });
+  });
+
+  describe('read a car', () => {
+    it('success', async () => {
+      const car = await carService.readOne('6353edb3665f07130107f630');
+      expect(car).to.be.deep.equal(carMockWithId);
     });
   });
 
